d403f1996dc468d3f4a2bcd91e510d019a371eb9
xiaoan-hu <xiaoan.hu@gmail.com>
2018-01-31 16:13:15 +0800

    用shiro完成登录页功能

---
+++ src/main/java/com/bdqn/t0724/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.bdqn.t0724.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(HttpServletRequest request,Model model) {
+
+        model.addAttribute("cardNo",request.getUserPrincipal().getName());
+
+        return "home-page";
+    }
+}
